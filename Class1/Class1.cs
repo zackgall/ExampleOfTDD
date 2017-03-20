68a040ceca8a9fa34533da0dbb1dbcc73729c911
Zack Gall <zackgall@gmail.com>
2017-03-20 10:18:18 -0400

    RomanTests TDD

---
+++ Class1/Class1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace RomanNumerals
+{
+    public class RomanConverter
+    {
+        public string GetRoman(int number)
+        {
+            string result = "";
+            while (number >= 100)
+            {
+                result += "C";
+                number -= 100;
+            }
+            if (number >= 90)
+            {
+                result += "XC";
+                number -= 90;
+            }
+            if (number >= 50)
+            {
+                result += "L";
+                number -= 50;
+            }
+            if (number >= 40)
+            {
+                result += "XL";
+                number -= 40;
+            }
+            while (number >= 10)
+            {
+                result += "X";
+                number -= 10;
+            }
+            if (number >= 9)
+            {
+                result += "IX";
+                number -= 9;
+            }
+            if (number >= 5)
+            {
+                result += "V";
+                number -= 5;
+            }
+            if (number >= 4)
+            {
+                result += "IV";
+                number -= 4;
+            }
+            while (number > 0)
+            {
+                result += "I";
+                number -= 1;
+            }
+            return result;
+        }
+    }
+}
---
+++ UnitTest1/UnitTest1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RomanNumerals;
+
+namespace Roman
+{
+    [TestClass]
+    public class RomanTest1
+    {
+        [TestMethod]
+        public void SanityCheck()
+        {
+            Assert.IsTrue(true);
+        }
+        [TestMethod]
+        public void Given_1_return_I()
+        {
+            // rc = RomanConverter.new in Ruby
+            RomanConverter rc = new RomanConverter();
+            Assert.AreEqual(rc.GetRoman(1), "I");
+        }
+        [TestMethod]
+        public void Given_2_return_II()
+        {
+            // rc = RomanConverter.new in Ruby
+            RomanConverter rc = new RomanConverter();
+            Assert.AreEqual(rc.GetRoman(2), "II");
+        }
+        [TestMethod]
+        public void Given_4_return_IV()
+        {
+            // rc = RomanConverter.new in Ruby
+            RomanConverter rc = new RomanConverter();
+            Assert.AreEqual(rc.GetRoman(4), "IV");
+        }
+    }
+}
